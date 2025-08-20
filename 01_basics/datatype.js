a1cba1aeb14858cfa84ccc43cc58bfb52004051a
amitkmaurya2114 <amitkmaurya1806@gmail.com>
2025-08-20 05:03:18 +0000

    datatype summary

---
+++ 01_basics/datatype.js
@@ -0,0 +1,25 @@
+// primitive
+
+// 7 type : String, Number, Boolean, null, undefined, Symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null //empty
+let userEmail;
+
+
+// reference (no-primitive)
+
+// Array, Objects, Functions
+
+
+const hero = ["Shaktimaan", "naagraj", "doga"]
+
+let myObj = {
+    name : "Amit",
+    age : 25,
+}
+
+
